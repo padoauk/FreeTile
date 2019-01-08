79d99147734af1940a85f053bcb40f55073afb7d
Toshinao.Ishii <padoauk@gmail.com>
2019-01-09 02:36:22 +0900

    set props in the sample

--- src/sample/vue/DashBoard.vue
+++ src/sample/vue/DashBoard.vue
@@ -3,14 +3,15 @@
   <div id="dashboard">
     <button v-on:click="addTile('App01', {left:200, top:200, width: 200, height: 150})">Add a App01</button>
     <button v-on:click="addTile('TextMsg', {left:100, top:100, width: 200, height: 150})">Add a TextMsg</button>
-    <TileArea></TileArea>
+    <TileArea v-bind:gridWpx="50" v-bind:gridHpx="50" v-bind:gridColNum="22" v-bind:gridRowNum="20">
+    </TileArea>
   </div>
 
 </div></template>
 
 <script>
-  import TileArea from '../../vue/TileArea.vue';
-  import { TileAreaEventBus } from '../../js/TileAreaEB';
+  import TileArea from './TileArea.vue';
+  import { TileAreaEventBus } from '../js/TileAreaEB';
   
   export default {
     name: "DashBoard",
